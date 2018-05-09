f46b8a4e6a7bbadcf6f9bfd3ff95babf147310e6
roguedevice <hall.jeffw@gmail.com>
2018-05-08 21:55:45 -0400

    Add edit action with tests.

---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "Olivia", email: "olivia@gmail.com",
+                        password: "password", password_confirmation: "password")
+  end
+
+  test "reject an invalid edit" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: '', email: ''}}
+    assert_template 'chefs/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test "accept valid edit" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: 'Maya', email: 'maya-dog@doggone.com'}}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "Maya", @chef.chefname
+    assert_match "maya-dog@doggone.com", @chef.email 
+  end
+end
--- test/integration/chefs_show_test.rb
+++ test/integration/chefs_show_test.rb
@@ -5,7 +5,7 @@ class ChefsShowTest < ActionDispatch::IntegrationTest
     @chef = Chef.create!(chefname: "Olivia", email: "olivia@gmail.com",
                         password: "password", password_confirmation: "password")
     @recipe = Recipe.create(name:"Spaghetti and Meatballs", description: "Great Italian classic. Ready in minutes.", chef: @chef)
-    @recipe2 = @chef.recipes.build(name: "Mac and Cheese", description: "Mac and cheese with smoked chicken.  It's bacon dance good!")
+    @recipe2 = @chef.recipes.build(name: "Mac and Cheese", description: "Mac and cheese with smoked chicken.  Its bacon dance good!")
     @recipe2.save
   end
 
@@ -15,7 +15,7 @@ class ChefsShowTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
     assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
     assert_match @recipe.description, response.body
-    # assert_match @recipe2.description, response.body
+    assert_match @recipe2.description, response.body
     assert_match @chef.chefname, response.body
   end
 end
